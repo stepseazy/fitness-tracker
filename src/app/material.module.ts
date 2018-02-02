cbfa8cb1f92c4619cca5470ca5c677d9272a7b0f
Rik Brutsaert <hb31@cornell.edu>
2018-02-02 10:01:08 -0800

    fixed sidenav and toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,7 +7,8 @@ import {MatButtonModule,
   MatNativeDateModule,
   MatCheckboxModule,
   MatSidenavModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatListModule
 } from '@angular/material';
 
 @NgModule({
@@ -19,7 +20,8 @@ import {MatButtonModule,
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   exports:[
     MatButtonModule,
@@ -30,7 +32,8 @@ import {MatButtonModule,
   MatNativeDateModule,
   MatCheckboxModule,
   MatSidenavModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatListModule
 ]
 })
 
