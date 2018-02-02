46eb68d2aab82c7ac6e4735af40ac803df280b54
Rik Brutsaert <hb31@cornell.edu>
2018-02-01 17:29:05 -0800

    added date picker validation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,28 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule} from '@angular/material';
+import {MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
   imports:[MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  exports:[MatButtonModule,
-  MatIconModule]
+  exports:[
+    MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+]
 })
 
 export class MaterialModule{}
