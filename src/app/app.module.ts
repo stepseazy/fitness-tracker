c33709f03dbfe5d2582d0497013dd411ba0f9f83
Rik Brutsaert <hb31@cornell.edu>
2018-02-05 09:04:46 -0800

    changed layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,9 @@ import { CurrentTrainingComponent } from './training/current-training/current-tr
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import {AppRoutingModule} from './app-routing.module'
+import {AppRoutingModule} from './app-routing.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import {AppRoutingModule} from './app-routing.module'
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
