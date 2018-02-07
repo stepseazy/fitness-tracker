ffbb9cf2c576ad9d8a9fd4c3f25c457962311924
Rik Brutsaert <hb31@cornell.edu>
2018-02-06 16:19:50 -0800

    added training service

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,EventEmitter, Output } from '@angular/core';
-
+import {TrainingService} from '../training.service';
+import {Exercise} from '../exercise.model';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart=new EventEmitter<void>();
-  constructor() { }
+  exercises:Exercise[];
+  constructor(private trainingService:TrainingService) { }
 
   ngOnInit() {
+    this.exercises=this.trainingService.getExercises();
   }
   onStartTraining(){
     this.trainingStart.emit();
